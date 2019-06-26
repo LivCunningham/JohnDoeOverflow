2d6f24091e403118e35fa96f21bfc6fd716695a2
Olivia Cunningham <Liv.cunninghamgb@gmail.com>
2019-06-26 10:43:38 -0400

    a little styling

--- ClientApp/src/Pages/PostQuestion.jsx
+++ ClientApp/src/Pages/PostQuestion.jsx
@@ -4,20 +4,22 @@ import './PostQuestion.css'
 class PostQuestion extends Component {
   render() {
     return (
-      <div>
-        <h2>POST YOUR QUESTION </h2>
-        <form>
-          <input
-            className="title-input"
-            type="text"
-            placeholder="Hey, JohnDoe! What's your question? Be specific. "
-          />
-          <textarea className="text-area" rows="8" cols="150">
-            Tell us more about your question here...
-          </textarea>
-          <button>Submit</button>
-        </form>
-      </div>
+      <section className="Post">
+        <div className="question-post">
+          <h2>POST YOUR QUESTION BELOW</h2>
+          <form className="input-Q">
+            <input
+              className="title-input"
+              type="text"
+              placeholder="Hey, JohnDoe! What's your question? Be specific. "
+            />
+            <textarea className="text-area" rows="10" cols="150">
+              Tell us more about your question here...
+            </textarea>
+            <button className="submit-Q">Submit</button>
+          </form>
+        </div>
+      </section>
     )
   }
 }
