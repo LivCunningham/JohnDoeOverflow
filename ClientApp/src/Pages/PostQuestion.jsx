932bdab735d2e59232cfc2d71b588249e49b105a
Olivia Cunningham <Liv.cunninghamgb@gmail.com>
2019-06-26 12:37:53 -0400

    post question styling

--- ClientApp/src/Pages/PostQuestion.jsx
+++ ClientApp/src/Pages/PostQuestion.jsx
@@ -2,6 +2,23 @@ import React, { Component } from 'react'
 import './PostQuestion.css'
 
 class PostQuestion extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+  }
+
+  //add new question post
+  addNewQuestion = event => {
+    this.setState({ value: event.target.value })
+  }
+
+  //submit form
+  submitNewQuestion = event => {
+    this.setState({ value: event.target.value })
+  }
+
   render() {
     return (
       <section className="Post">
@@ -12,11 +29,20 @@ class PostQuestion extends Component {
               className="title-input"
               type="text"
               placeholder="Hey, JohnDoe! What's your question? Be specific. "
+              onChange={this.addNewQuestion}
+              value={this.state.value}
             />
-            <textarea className="text-area" rows="10" cols="150">
-              Tell us more about your question here...
-            </textarea>
-            <button className="submit-Q">Submit</button>
+            <textarea
+              className="text-area"
+              rows="10"
+              cols="150"
+              onChange={this.addNewQuestion}
+              value={this.state.value}
+              placeholder="Tell us more about your question here..."
+            />
+            <button className="submit-Q">
+              <h3>Submit</h3>
+            </button>
           </form>
         </div>
       </section>
