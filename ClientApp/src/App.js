c3afd5f73ad61ccc77aab09e8f87c2e1d82eb819
Allan Hall <bigal76126@gmail.com>
2019-06-27 17:04:57 -0400

    trying to pull

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router'
 import { Layout } from './components/Layout'
 import Home from './Pages/Home.jsx'
 import PostQuestion from './Pages/PostQuestion.jsx'
+import Questions from './Pages/Questions.jsx'
 
 export default class App extends Component {
   static displayName = App.name
@@ -12,6 +13,7 @@ export default class App extends Component {
       <Layout>
         <Route exact path="/" component={Home} />
         <Route exact path="/Post" component={PostQuestion} />
+        <Route exact path="/questions/:id" component={Questions} />
       </Layout>
     )
   }
--- ClientApp/src/Pages/Questions.jsx
+++ ClientApp/src/Pages/Questions.jsx
@@ -1,9 +1,73 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
 
-class Questions extends Component {
-  render() {
-    return <div />
+export default function Questions(props) {
+  const [question, setQuestion] = useState({})
+  const [answers, setAnswers] = useState([])
+  const [voteCount, setVoteCount] = useState(0)
+  const qID = props.match.params.id
+  useEffect(() => {
+    Axios.get(`api/questions/${qID}`).then(resp => {
+      setQuestion({
+        title: resp.data.title,
+        description: resp.data.description,
+        qVoteCount: resp.data.voteCount
+      })
+      setAnswers({
+        answers: resp.data.answers,
+        aVoteCount: resp.data.voteCount
+      })
+    })
+  }, [])
+
+  const upvote = () => {
+    Axios.put(`/api/answers/voteA/${qID}`)
+    answers.aVoteCount = answers.VoteCount + 1
   }
-}
 
-export default Questions
+  const downvote = () => {
+    Axios.put(`/api/answers/voteA/${qID}`)
+    answers.aVoteCount = answers.VoteCount + 1
+  }
+
+  const submitAnswer = e => {
+    e.preventDefault()
+    Axios.post('/api/answers')
+  }
+
+  const updateValue = e => {
+    const state = this.state
+    state.question[e.target.name] = e.target.value
+    this.setState(state)
+  }
+
+  return (
+    <section className="Post">
+      <div className="question-post">
+        <h1>{question.title}</h1>
+        <p>{question.description}</p>
+        <form onSubmit={() => submitAnswer()}>
+          <textarea
+            className="text-area"
+            rows="8"
+            cols="100"
+            onChange={() => updateValue()}
+            placeholder="Answer this question"
+            name="description"
+          />
+        </form>
+        <div>
+          {answers.map(answer => {
+            return (
+              <div>
+                <div>{answer.description}</div>
+                <button onClick={() => upvote()}>UpVote</button>
+                <button onClick={() => downvote()}>DownVote</button>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </section>
+  )
+}
